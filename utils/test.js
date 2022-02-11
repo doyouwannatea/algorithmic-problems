8d38b32bbe2ec9078901e731e0d005799dabcf97
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-02-11 20:12:50 +0800

    test.js update

--- utils/test.js
+++ utils/test.js
@@ -3,13 +3,8 @@ const { red, green, bold } = require('colors');
 function test(expect, answer) {
   const equal = JSON.stringify(expect) === JSON.stringify(answer);
   console.log(
-    `answer ${bold(answer)} is ${
-      equal ? green('right') : red.underline('wrong')
-    }`
+    `expect: ${bold(expect)}  answer: ${equal ? green(answer) : red(answer)}`
   );
-  if (!equal) {
-    console.log(`${green.underline('right')} answer is ${bold(expect)}`);
-  }
   return equal;
 }
 
