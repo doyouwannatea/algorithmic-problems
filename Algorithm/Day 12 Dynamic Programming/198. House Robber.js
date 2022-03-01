58da4b73e89d0597cf16eaea374a43c8ed129f3f
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-03-01 17:14:35 +0800

    перерешал 198. house robber

--- Algorithm/Day 12 Dynamic Programming/198. House Robber.js
+++ Algorithm/Day 12 Dynamic Programming/198. House Robber.js
@@ -3,35 +3,15 @@
  * @return {number}
  */
 function rob(nums) {
-  // Рекурсия с мемоизацией
-  //   /**
-  //    * @param {number[]} nums
-  //    * @param {number} idx
-  //    * @param {Map<number, number>} hash
-  //    * @returns {number}
-  //    */
-  //   function rec(nums, idx, hash) {
-  //     if (idx >= nums.length) return 0;
-  //     if (hash.has(idx)) return hash.get(idx);
-  //     const res = Math.max(
-  //       nums[idx] + rec(nums, idx + 2, hash),
-  //       rec(nums, idx + 1, hash)
-  //     );
-  //     return hash.set(idx, res).get(idx);
-  //   }
-  //   return rec(nums, 0, new Map());
-  //
-  //   Динамическое программирование
-  if (nums.length === 1) return nums[0];
-  let prev2 = nums[0],
-    prev1 = Math.max(nums[0], nums[1]),
-    curr = prev1;
-  for (let i = 2; i < nums.length; i++) {
-    curr = Math.max(prev1, prev2 + nums[i]);
-    prev2 = prev1;
-    prev1 = curr;
+  let [prev1, prev2] = [0, 0];
+
+  for (const num of nums) {
+    const tmp = prev1;
+    prev1 = Math.max(prev1, prev2 + num);
+    prev2 = tmp;
   }
-  return curr;
+
+  return prev1;
 }
 
 // 4
@@ -45,3 +25,6 @@ console.log(rob([1000, 1, 1, 1000, 200, 3, 1, 3333]));
 
 // 1000
 console.log(rob([1000]));
+
+// 0
+console.log(rob([]));
