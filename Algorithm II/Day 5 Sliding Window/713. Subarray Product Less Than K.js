496f680f3e576392d324cc975f00680f3f5bf900
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-03-07 22:47:43 +0800

    оптимизировал 713, но всё равно не проходит по времени :(

--- Algorithm II/Day 5 Sliding Window/713. Subarray Product Less Than K.js
+++ Algorithm II/Day 5 Sliding Window/713. Subarray Product Less Than K.js
@@ -1,4 +1,5 @@
 const testcase35 = require('./713. test-35.json');
+const testcase42 = require('./713. test-42.json');
 
 /**
  * @param {number[]} nums
@@ -10,18 +11,21 @@ function numSubarrayProductLessThanK(nums, k) {
     r = 0,
     count = 0;
 
-  const product = (arr) => arr.reduce((a, b) => a * b, 1);
+  const hash = new Map();
 
   while (l >= 0 || r < nums.length) {
     while (r < nums.length) {
-      if (product(nums.slice(l, r + 1)) < k) count++;
+      if (l === r) hash.set(l + r - 1, 1);
+      hash.set(l + r, hash.get(l + r - 1) * nums[r]);
+      if (hash.get(l + r) < k) count++;
       l++;
       r++;
     }
     l -= 2;
     r -= 1;
     while (l >= 0) {
-      if (product(nums.slice(l, r + 1)) < k) count++;
+      hash.set(l + r, hash.get(l + r + 1) * nums[l]);
+      if (hash.get(l + r) < k) count++;
       r--;
       l--;
     }
@@ -37,7 +41,10 @@ function numSubarrayProductLessThanK(nums, k) {
 console.log(numSubarrayProductLessThanK([10, 5, 2, 6], 100));
 
 // Output: 0
-console.log(numSubarrayProductLessThanK([1, 2, 3], 0));
+// console.log(numSubarrayProductLessThanK([1, 2, 3], 0));
 
 // Output: ?
-console.log(numSubarrayProductLessThanK(testcase35.nums, testcase35.k));
+// console.log(numSubarrayProductLessThanK(testcase35.nums, testcase35.k));
+
+// Output: ?
+console.log(numSubarrayProductLessThanK(testcase42.nums, testcase42.k));
