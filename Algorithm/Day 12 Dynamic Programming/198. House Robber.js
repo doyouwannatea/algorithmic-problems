057617e458b32df885c201b9f20ed3aba40e5f5e
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-03-12 17:44:51 +0800

    перерешал 198. House robber

--- Algorithm/Day 12 Dynamic Programming/198. House Robber.js
+++ Algorithm/Day 12 Dynamic Programming/198. House Robber.js
@@ -1,17 +1,20 @@
+// @ts-check
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
 function rob(nums) {
-  let [prev1, prev2] = [0, 0];
-
-  for (const num of nums) {
-    const tmp = prev1;
-    prev1 = Math.max(prev1, prev2 + num);
-    prev2 = tmp;
+  if (nums.length === 0) return 0;
+  if (nums.length < 3) return Math.max(...nums);
+  const hash = new Map();
+  hash.set(0, nums[0]);
+  hash.set(1, Math.max(nums[0], nums[1]));
+  for (let i = 2; i < nums.length; i++) {
+    hash.set(i, Math.max(hash.get(i - 1), hash.get(i - 2) + nums[i]));
   }
 
-  return prev1;
+  return hash.get(hash.size - 1);
 }
 
 // 4
@@ -25,6 +28,9 @@ console.log(rob([1000, 1, 1, 1000, 200, 3, 1, 3333]));
 
 // 1000
 console.log(rob([1000]));
+
+// 2000
+console.log(rob([1000, 2000]));
 
 // 0
 console.log(rob([]));
